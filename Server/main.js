c8526e684ad69db5322913531bc31d208f9aa0f8
Roy <MekonenRoy.com>
2022-12-16 15:47:03 +0200

    my update

--- Server/main.js
+++ Server/main.js
@@ -3,10 +3,15 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 require('./config/database');
+const depatemrntRoute = require('./routers/departmentRouter.js')
 
 app.use(cors());
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
+
+app.use('/department',depatemrntRoute);
+
+
 
 let port = 8000;
 app.listen(port, () => {
---
+++ client/src/compontes/main/Departments.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+
+export default function Departments() {
+    const [departments,setDepartments]=useState([])
+  return (
+    <div>Departments</div>
+  )
+}
