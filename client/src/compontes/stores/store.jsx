938fec9cd97efd3f9d55136437da6f4ffac694b8
Roy <MekonenRoy.com>
2022-12-23 15:56:09 +0200

    update

--- client/src/compontes/stores/store.jsx
+++ client/src/compontes/stores/store.jsx
@@ -13,7 +13,7 @@ export default function Store(props) {
 
   return (
     <div>
-      <div className="flex flex-col sm:flex-row items-center sm:items-end  w-40 gap-4 p-2">
+      {/* <div className="flex flex-col sm:flex-row items-center sm:items-end  w-40 gap-4 p-2">
         <img src={props.store.coverImage.url} alt="" />
         <div>
           <h4>
@@ -34,31 +34,21 @@ export default function Store(props) {
           </div>
         </div>
         <button className="store-btn">קנה בחנות זו</button>
-      </div>
-      {/* <section className="text-gray-700 body-font overflow-hidden bg-white">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="lg:w-4/5 mx-auto flex flex-wrap">
-            <img
-              alt="ecommerce"
-              className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200"
-              src={props.store.coverImage.url}
-            />
-            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-              <h2 className="text-sm title-font text-gray-500 tracking-widest">
-              {props.store.name}
-              </h2>
-              <p className="text-gray-900  title-font font-medium mb-1">{props.store.description}
-              </p>
-              <div className="flex mb-4">
-                <span className="flex items-center">
-                  <span className="text-gray-600 ml-3">{props.store.address.city},</span>
-                  <span className="text-gray-600 ml-3">{props.store.address.street} {props.store.address.building}</span>
-                </span>
-              </div>
-            </div>
+      </div> */}
+      <div className="flex justify-center">
+        <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+          <img className=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src={props.store.coverImage.url} alt={props.store.name} />
+          <div className="p-6 flex flex-col justify-start">
+            <div className='flex flex-row justify-between'><h5 className="text-gray-900 text-xl font-medium mb-2">{props.store.name}</h5> <img className='w-9' src={props.store.lightlogo.url} alt="" /></div>
+            <p className="text-gray-700 text-base mb-4">
+            {props.store.description}
+            </p>
+            <p className="text-gray-600 text-xs">{props.store.address.city}, {props.store.address.street} {props.store.address.building} </p>
           </div>
+          <button className="store-btn">קנה בחנות זו</button>
+
         </div>
-      </section> */}
+      </div>
     </div>
   );
 }
